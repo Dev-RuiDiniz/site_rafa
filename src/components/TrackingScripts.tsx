100270a14e02bc7d691028edb99e30c65b1086fa
Wbianchi <willianbianchirocha1996@gmail.com>
2026-02-09 10:03:27 -0300

    fix: corrigir scripts de tracking para funcionar com SSR e Search Console

--- src/components/TrackingScripts.tsx
+++ src/components/TrackingScripts.tsx
@@ -13,66 +13,92 @@ const TRACKING_CONFIG = {
   maletti: {
     gtmId: "GTM-NHTPW27K",
     ga4Id: "G-SCQRQ9F9LD",
-    metaPixelId: null, // Maletti não tem Meta Pixel por enquanto
+    metaPixelId: null as string | null,
     siteVerification: "SkrI3t5Q5vQ_OvnnTNksc-gx1nKisw0Gq0oANsuLvM0",
   },
 };
 
+// Detectar domínio de forma que funcione no client
+function getTrackingConfig() {
+  if (typeof window === "undefined") {
+    // SSR: retorna config padrão (SHR) - será atualizado no client
+    return { config: TRACKING_CONFIG.shr, isMaletti: false };
+  }
+  const hostname = window.location.hostname;
+  const isMalettiDomain = hostname.includes("maletti");
+  return {
+    config: isMalettiDomain ? TRACKING_CONFIG.maletti : TRACKING_CONFIG.shr,
+    isMaletti: isMalettiDomain,
+  };
+}
+
 export function TrackingScripts() {
-  const [config, setConfig] = useState<typeof TRACKING_CONFIG.shr | typeof TRACKING_CONFIG.maletti | null>(null);
-  const [isMaletti, setIsMaletti] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  const [trackingState, setTrackingState] = useState(() => getTrackingConfig());
 
   useEffect(() => {
-    const hostname = window.location.hostname;
-    const isMalettiDomain = hostname.includes("maletti");
-    setIsMaletti(isMalettiDomain);
-    setConfig(isMalettiDomain ? TRACKING_CONFIG.maletti : TRACKING_CONFIG.shr);
+    setMounted(true);
+    // Atualiza config no client para o domínio correto
+    setTrackingState(getTrackingConfig());
   }, []);
 
-  if (!config) return null;
+  // Renderiza os scripts imediatamente com config padrão
+  // O script vai detectar o domínio correto no client
+  const { config, isMaletti } = trackingState;
 
   return (
     <>
-      {/* Google Tag Manager */}
+      {/* Google Tag Manager - detecta domínio dinamicamente */}
       <Script
         id="gtm-script"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${config.gtmId}');
+            (function(){
+              var isMaletti = window.location.hostname.includes('maletti');
+              var gtmId = isMaletti ? 'GTM-NHTPW27K' : 'GTM-M4LJXG68';
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer',gtmId);
+            })();
           `,
         }}
       />
 
-      {/* Google Analytics 4 */}
+      {/* Google Analytics 4 - detecta domínio dinamicamente */}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${config.ga4Id}`}
-        strategy="afterInteractive"
-      />
-      <Script
-        id="ga4-script"
+        id="ga4-loader"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${config.ga4Id}');
+            (function(){
+              var isMaletti = window.location.hostname.includes('maletti');
+              var ga4Id = isMaletti ? 'G-SCQRQ9F9LD' : 'G-Z2EQWFFMQC';
+              var script = document.createElement('script');
+              script.src = 'https://www.googletagmanager.com/gtag/js?id=' + ga4Id;
+              script.async = true;
+              document.head.appendChild(script);
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              window.gtag = gtag;
+              gtag('js', new Date());
+              gtag('config', ga4Id);
+            })();
           `,
         }}
       />
 
-      {/* Meta Pixel - apenas para SHR (carrega após página completa) */}
-      {!isMaletti && config.metaPixelId && (
-        <Script
-          id="meta-pixel"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
+      {/* Meta Pixel - apenas para SHR */}
+      <Script
+        id="meta-pixel"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(){
+              var isMaletti = window.location.hostname.includes('maletti');
+              if (isMaletti) return; // Maletti não tem Meta Pixel
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -81,53 +107,53 @@ export function TrackingScripts() {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${config.metaPixelId}');
+              fbq('init', '1609509633385345');
               fbq('track', 'PageView');
-            `,
-          }}
-        />
-      )}
+            })();
+          `,
+        }}
+      />
     </>
   );
 }
 
 export function TrackingNoscript() {
-  const [config, setConfig] = useState<typeof TRACKING_CONFIG.shr | typeof TRACKING_CONFIG.maletti | null>(null);
-  const [isMaletti, setIsMaletti] = useState(false);
-
-  useEffect(() => {
-    const hostname = window.location.hostname;
-    const isMalettiDomain = hostname.includes("maletti");
-    setIsMaletti(isMalettiDomain);
-    setConfig(isMalettiDomain ? TRACKING_CONFIG.maletti : TRACKING_CONFIG.shr);
-  }, []);
-
-  if (!config) return null;
-
+  // Renderiza ambos os noscripts - o browser vai ignorar o que não for do domínio
+  // O noscript do GTM é necessário para validação do Search Console
   return (
     <>
-      {/* GTM noscript */}
+      {/* GTM noscript - SHR */}
       <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${config.gtmId}`}
+          src="https://www.googletagmanager.com/ns.html?id=GTM-M4LJXG68"
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
+          title="GTM SHR"
+        />
+      </noscript>
+
+      {/* GTM noscript - Maletti */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NHTPW27K"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+          title="GTM Maletti"
         />
       </noscript>
 
       {/* Meta Pixel noscript - apenas SHR */}
-      {!isMaletti && config.metaPixelId && (
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${config.metaPixelId}&ev=PageView&noscript=1`}
-            alt=""
-          />
-        </noscript>
-      )}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1609509633385345&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
     </>
   );
 }
